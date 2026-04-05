a179e988905ed8d6eed814ead8983aa9076c1f06
thanh-ndt <thanhndt2004@gmail.com>
2026-04-06 00:56:45 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config();
 
 const connectDB = require('./src/congfig/db');
